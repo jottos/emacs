e959756bc9542a8bc691d9e06acb2365aa5229cb
john schneider <jos@jos-land.local>
2013-09-28 16:28:29 -0700

    use (kbd ...) working on prod emacs file

--- scala-mode-settings.el
+++ scala-mode-settings.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/scala-mode2")
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.sc\\'" . scala-mode))
 (require 'scala-mode2)
 (provide 'scala-mode-settings)
